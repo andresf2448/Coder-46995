f56b5273a74c34dd0dcfe2a5a863dcda11842d1c
andresf2448 <andresf2448@gmail.com>
2023-07-05 20:08:16 -0500

    Clase 1

---
+++ Clase 1/main.js
@@ -0,0 +1,55 @@
+// comentario de linea
+
+/* 
+comentario
+de
+linea
+*/
+
+// let edad = 18;
+//forma recomendable
+// let nombre = "andres";
+
+// let nombreBonitoGrande;
+
+//no recomendable
+// var cantidad = 10;
+
+//camelCase
+//PascalCase
+//snake_case
+
+// const EDAD = 5;
+// EDAD = 10;
+
+// let numeroUno = 5;
+// let numeroDos = 10;
+
+// let resultadoSuma = numeroUno + numeroDos;
+// let resultadoResta = numeroUno - numeroDos;
+// let resultadoMultiplicacion = numeroUno * numeroDos;
+// let resultadoDivision = numeroUno / numeroDos;
+
+// console.log("Aca la suma", resultadoSuma);
+// console.log("Aca la resta", resultadoResta);
+// console.log("Aca la multiplicacion", resultadoMultiplicacion);
+// console.log("Aca la division", resultadoDivision);
+
+// let saludo = "Hola";
+// let nombre = " camila";
+
+// let saludoCompleto = saludo + nombre;
+// console.log(saludoCompleto);
+
+//prompt
+// let nombre = prompt("Ingrese su nombre");
+// alert("Hola " + nombre);
+
+let numero = parseInt(prompt("Ingrese un número"));
+alert(numero + 5);
+
+// let numero = 8;
+// console.log(typeof numero);
+
+// let nombre = "camila";
+// console.log(typeof(nombre));
