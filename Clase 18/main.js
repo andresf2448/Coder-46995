819935a6afeca9f0a6f10e7872070b83a7ca6a83
andresf2448 <andresf2448@gmail.com>
2023-09-04 19:46:50 -0500

    Clase 18

---
+++ Clase 18/main.js
@@ -0,0 +1,8 @@
+const { randomMsg } = require("random-messages-random");
+
+console.log(randomMsg());
+console.log(randomMsg());
+console.log(randomMsg());
+console.log(randomMsg());
+console.log(randomMsg());
+console.log(randomMsg());
